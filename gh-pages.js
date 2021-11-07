ff83f30d0eccce750a497bf522911de018c5ea40
sean-cunniffe <A00290418@student.ait.ie>
2021-11-07 18:33:44 +0000

    Added gh-pages support

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+	'public', // path to public directory
+	{
+		branch: 'gh-pages',
+		repo: 'https://github.com/sean-cunniffe/svelte-portfolio', // Update to point to your repository
+		user: {
+			name: 'sean.cunniffe', // update to use your name
+			email: 'sean.cunniffe927@gmail.com' // Update to use your email
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
